6296ae5b0b9e4eeea3884f49eff95dc835a34e82
Chris Connett <cjc@google.com>
2020-02-12 11:50:58 -0500

    Add function to track cube list over time.

--- extract_changes.py
+++ extract_changes.py
@@ -1,10 +1,15 @@
+"""Track cube changes over time."""
+import collections
 import datetime
+import itertools
 import os
+import pprint
 
 import bs4
 
 
 def GetChanges():
+  """Extract changes from CubeTutor blog html files."""
   results = []
   for name in os.listdir('ctdata'):
     if name.endswith('.html'):
@@ -26,5 +31,33 @@ def GetChanges():
   return sorted(results, reverse=True)
 
 
-import pprint
-pprint.pprint(GetChanges())
+# pprint.pprint(GetChanges(), stream=open('changes.txt', 'w'))
+
+
+def TrackCube():
+  """Construct the intermediate lists."""
+  all_lists = {}
+  counter = collections.Counter(
+      line.strip() for line in open('ChrissVintageCube.txt'))
+  changes = GetChanges()
+  for stamp, g in itertools.groupby(changes, key=lambda c: c[0]):
+    all_lists[stamp] = counter.copy()
+    for change in g:
+      _, mode, card = change
+      # We're going backwards from the most recent list; operations are reversed
+      # from their natural descriptions.
+      if mode in ('SIDEBOARD', 'REMOVE'):
+        counter.update([card])
+      elif mode == 'ADD':
+        counter.subtract([card])
+      elif mode == 'ADD_TO_SIDEBOARD':
+        pass
+      else:
+        raise ValueError('Unknown mode.')
+  all_lists[datetime.datetime.fromtimestamp(0)] = counter.copy()
+  return all_lists
+
+
+x = TrackCube()
+import IPython
+IPython.embed()
